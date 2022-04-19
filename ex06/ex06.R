64a3a2285bcc82d23e4c960cf48a8d66b446fb75
Emily Hsiao <emilyhsiao@wireless-10-147-35-221.public.utexas.edu>
2022-04-19 12:07:40 -0500

    gaussian processes code and images

--- ex06/ex06.R
+++ ex06/ex06.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(mvtnorm)
 
 # Takes in a dataset and a kernel function
 # Outputs a function which is the kernel smoother
@@ -40,7 +41,7 @@ data %>%
 smoother <- smoother_fitter(x, y, gaussian_kernel, 0.5)
 x_new = seq(-5, 5, by=0.01)
 
-h_values = c(0.1, 0.5, 1, 5, 10)
+h_values = c(0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 5, 10)
 pred_mat <- matrix(data=NA, nrow=length(x_new), ncol=length(h_values))
 for (i in 1:length(h_values)) {
   smoother <- smoother_fitter(x, y, gaussian_kernel, h_values[i])
@@ -88,8 +89,8 @@ create_test_train <- function(n,
                               test_prop, 
                               fn, 
                               sd = 1, 
-                              low = 0, 
-                              high = 1) {
+                              low = -5, 
+                              high = 5) {
   x_train = runif(n * (1 - test_prop), low, high)
   exact_train = fn(x_train)
   noisy_train = sapply(exact_train, function(val) rnorm(1, val, sd))
@@ -111,14 +112,14 @@ test <- dataset$test
 results <- train_test_smoother(train, test, gaussian_kernel)
 
 wiggly <- function(x) sin(10 * x)
-smooth <- function(x) x^3
+smooth <- function(x) x^3 / 12
 
-wiggly_noisy <- create_test_train(500, 0.2, wiggly, sd = 1)
-smooth_noisy <- create_test_train(500, 0.2, smooth, sd = 1)
-wiggly_quiet <- create_test_train(500, 0.2, wiggly, sd = 0.1)
-smooth_quiet <- create_test_train(500, 0.2, smooth, sd = 0.1)
+wiggly_noisy <- create_test_train(500, 0.2, wiggly, sd = 2)
+smooth_noisy <- create_test_train(500, 0.2, smooth, sd = 2)
+wiggly_quiet <- create_test_train(500, 0.2, wiggly, sd = 0.4)
+smooth_quiet <- create_test_train(500, 0.2, smooth, sd = 0.4)
 
-h_values = seq(0.1, 3, by=0.1)
+h_values = c(seq(0.1, 3, by=0.1), 4, 5, 6, 10)
 
 # Helper functions
 find_optimal_h <- function(dataset, kernel, h = c(0.1, 0.5, 1, 5, 10)) {
@@ -133,7 +134,7 @@ find_optimal_h <- function(dataset, kernel, h = c(0.1, 0.5, 1, 5, 10)) {
   list("fun" = best_fn, "error" = best_error, "h" = best_h)
 }
 
-create_line_df <- function(fun, low = 0, high = 1, by = 0.01) {
+create_line_df <- function(fun, low = -5, high = 5, by = 0.01) {
   x_grid <- seq(low, high, by = by)
   y_vals <- sapply(x_grid, fun)
   data.frame(x_grid, y_vals)
@@ -147,6 +148,7 @@ ggplot() +
   geom_point(data=wn_test_df, aes(x=x_test, y=noisy_test), size=0.5, alpha=0.5) +
   geom_line(data=wn_line_df, aes(x=x_grid, y=y_vals))
 results$h
+ggsave("./Desktop/Sp22/modeling/SDS383c/ex06/wiggly_noisy.png")
 # for wiggly and noisy, the best h = 0.1
 
 # Wiggly and not-so-noisy
@@ -157,6 +159,7 @@ ggplot() +
   geom_point(data=wq_test_df, aes(x=x_test, y=noisy_test), size=0.5, alpha=0.5) +
   geom_line(data=wq_line_df, aes(x=x_grid, y=y_vals))
 results2$h
+ggsave("./Desktop/Sp22/modeling/SDS383c/ex06/wiggly_quiet.png")
 # here the best h = 0.1 too
 
 # Smooth and Noisy
@@ -167,6 +170,7 @@ ggplot() +
   geom_point(data=sn_test_df, aes(x=x_test, y=noisy_test), size=0.5, alpha=0.5) +
   geom_line(data=sn_line_df, aes(x=x_grid, y=y_vals))
 results3$h
+ggsave("./Desktop/Sp22/modeling/SDS383c/ex06/smooth_noisy.png")
 # h = 0.1
 
 # Smooth and not-so-noisy
@@ -177,6 +181,7 @@ ggplot() +
   geom_point(data=sq_test_df, aes(x=x_test, y=noisy_test), size=0.5, alpha=0.5) +
   geom_line(data=sq_line_df, aes(x=x_grid, y=y_vals))
 results4$h
+ggsave("./Desktop/Sp22/modeling/SDS383c/ex06/smooth_quiet.png")
 # h = 0.1
 
 # Using leave one out
@@ -213,5 +218,117 @@ colnames(errors) = c("wiggly-noisy",
                      "wiggly-quiet", 
                      "smooth-noisy", 
                      "smooth-quiet")
+write.csv(data.frame(errors), "./Desktop/Sp22/modeling/SDS383c/ex06/loocv.csv")
 
 
+# Local polynomial
+utilities <- read.csv("./Desktop/Sp22/modeling/SDS383D-master/data/utilities.csv")
+x = utilities$temp
+y = utilities$gasbill / utilities$billingdays
+H = x %*% solve(t(x) %*% x) %*% t(x)
+
+sj <- function(x_new, h, j) {
+  sum = 0
+  for (i in 1:n) {
+    int = (x_new - X[i]) / h
+    sum = sum + gaussian_kernel(int) * (X[i] - x_new)^j
+  }
+  sum
+}
+
+w <- function(x_new, x_old, h) {
+  x_diff = (x_new - x_old) / h
+  s1 = sj(x_new, h, 1)
+  s2 = sj(x_new, h, 2)
+  gaussian_kernel(x_diff) * (s2 - (x_old - x_new) * s1)
+}
+
+# Gaussian Process
+
+# Create covariance matrix with Matern function (squared exponential)
+matern <- function(x1, x2, b, tau1.sq, tau2.sq) {
+  distance = dist(rbind(x1, x2))
+  ind = as.integer(all(x1==x2))
+  tau1.sq*exp(-.5*(distance/b)^2) + tau2.sq*ind
+}
+
+# Create x_1,...,x_N
+n <- 500
+x_values <- sort(runif(n, 0, 1))
+
+gaussian_process <- function(x, 
+                             f = matern, # covariance function
+                             m = function(x) 0, # mean function
+                             b = 1, 
+                             tau1.sq = 1, 
+                             tau2.sq = 1
+                             ) {
+  n = length(x)
+  covariance = matrix(data=NA, nrow=n, ncol=n)
+  for (i1 in 1:n) {
+    for (i2 in 1:n) {
+      x1 = x[i1]
+      x2 = x[i2]
+      # fill in covariance matrix
+      covariance[i1,i2] = matern(x1, x2, b, tau1.sq, tau2.sq)
+    }
+  }
+  Y = rmvnorm(n=1, mean=sapply(x, m), sigma=covariance)
+  Y
+}
+
+tau2 = 1e-6
+tau1 = seq(0, 2, length.out = 10)
+b = seq(0.0001, 1, length.out = 10)
+iters = 3
+
+for (b_i in b) {
+  df = data.frame(x_values)
+  names = c()
+  for (t1 in tau1) {
+    y = gaussian_process(x_values, b=b_i, tau1.sq = t1, tau2.sq=tau2)
+    t1_round = round(t1, 2)
+    name = paste("tau_1^2 =", t1_round)
+    df[name] = as.vector(y)
+    names = append(names, name)
+  }
+  # Create and save plot
+  plot.df = df %>% gather(key = "pars", value = "value", -1)
+  p = ggplot(plot.df, aes(x = x_values, y = value)) + 
+    geom_line(aes(color = pars)) + 
+    theme_classic() + 
+    ggtitle(paste0("Gaussian Processes (b = ", b_i, ")")) + xlab("x")
+  bi_title = str_replace(toString(b_i), pattern=".", replacement="_")
+  plot_title = paste("b", bi_title, ".jpg", sep="")
+  ggsave(filename=paste("./Desktop/Sp22/modeling/SDS383c/ex06/", plot_title,sep=""),
+         plot=p)
+}
+
+
+matern_52 <- function(x1, x2, b, tau1.sq, tau2.sq) {
+  d =  dist(rbind(x1, x2))
+  ind = as.integer(all(x1==x2))
+  tau1.sq*(1 + sqrt(5)*d/b + 5*d^2/(3*b^2))*exp(-sqrt(5)*d/b)+tau2.sq*ind
+}
+
+for (b_i in b) {
+  df = data.frame(x_values)
+  names = c()
+  for (t1 in tau1) {
+    y = gaussian_process(x_values, f=matern_52, b=b_i, tau1.sq = t1, tau2.sq=tau2)
+    t1_round = round(t1, 2)
+    name = paste("tau_1^2 =", t1_round)
+    df[name] = as.vector(y)
+    names = append(names, name)
+  }
+  # Create and save plot
+  plot.df = df %>% gather(key = "pars", value = "value", -1)
+  p = ggplot(plot.df, aes(x = x_values, y = value)) + 
+    geom_line(aes(color = pars)) + 
+    theme_classic() + 
+    ggtitle(paste0("Gaussian Processes (b = ", b_i, ")")) + xlab("x")
+  bi_title = str_replace(toString(b_i), pattern=".", replacement="_")
+  plot_title = paste("b", bi_title, ".jpg", sep="")
+  ggsave(filename=paste("./Desktop/Sp22/modeling/SDS383c/ex06/matern52", plot_title,sep=""),
+         plot=p)
+}
